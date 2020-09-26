af590386a1acb8a1cd554de1d9ec22794f3a12df
Tommy Kovalchuk <34948289+tommypk17@users.noreply.github.com>
2020-09-25 22:28:41 -0400

    allow lazy-load of profile data

--- angular/src/app/modules/profile/components/profile/profile.component.ts
+++ angular/src/app/modules/profile/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -9,14 +10,15 @@ export class ProfileComponent implements OnInit {
 
   headerBlock = {blockTitle: "Task Tracker - Profile", blockSubtitle: "", blockContent: ""};
 
-  profile: Profile;
+  profile = new Profile();
 
-  constructor() {
-  }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     //TODO: grab user profile info from db
-    this.profile = new Profile("Tom", "K", "tomko", "tom@tom.com");
+    this.authService.getProfile().subscribe((res: AuthService) => {
+      this.profile = new Profile(res.data.firstname, res.data.lastname, res.data.username, res.data.email);
+    });
   }
 
   deleteAccount(){
@@ -31,7 +33,7 @@ class Profile {
   public username: string;
   public email: string;
 
-  constructor(firstname: string, lastname: string, username: string, email: string){
+  constructor(firstname?: string, lastname?: string, username?: string, email?: string){
     this.firstname = firstname;
     this.lastname = lastname;
     this.username = username;
