cefffc53139bfdce3d6a1a225bb767bc6a7adee3
Tommy Kovalchuk <34948289+tommypk17@users.noreply.github.com>
2020-10-04 19:15:17 -0400

    toolbar, finalize for pre-release

--- angular/src/app/shared/toolbar/toolbar.component.ts
+++ angular/src/app/shared/toolbar/toolbar.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {TaskList} from '../shared.module';
 import {TasksService} from '../../services/tasks.service';
+import {AuthService} from '../../services/auth.service';
+import {NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,27 +11,46 @@ import {TasksService} from '../../services/tasks.service';
 })
 export class ToolbarComponent implements OnInit {
 
-  recentTaskLists: TaskList[] = [];
+  public isUserLoggedIn: boolean = false;
+  @Input() recentTaskLists: TaskList[] = [];
 
-  constructor(private taskService: TasksService) {}
+  constructor(private taskService: TasksService, private authService: AuthService, private router: Router) {
+    this.isUserLoggedIn = this.authService.loggedIn();
+    this.updateRecentTaskLists();
+  }
 
   ngOnInit(): void {
-    //TODO: grab recent task lists from db (3 max)
-    this.taskService.getRecentTaskLists().subscribe((res: TasksService) => {
-      if(res.success){
-        const tasklists = res.data;
-        let count = 0;
-        for(let tasklist of tasklists){
-          if(count < 3){
-            this.recentTaskLists.push({id: tasklist._id, title: tasklist.title, subtitle: tasklist.subtitle, content: tasklist.content, link: '/task-list/'+tasklist._id});
-          }else{
-            break;
-          }
-          count++;
-        }
+    this.taskService.updatedRecentTaskLists.subscribe((res) => {
+      if(res){
+        this.updateRecentTaskLists();
       }
     });
   }
 
+
+  updateRecentTaskLists():void{
+    if(this.isUserLoggedIn){
+      this.taskService.getRecentTaskLists().subscribe((res: TasksService) => {
+        if(res.success){
+          this.recentTaskLists = [];
+          const tasklists = res.data;
+          let count = 0;
+          for(let tasklist of tasklists){
+            if(count < 3){
+              this.recentTaskLists.push({id: tasklist._id, title: tasklist.title, subtitle: tasklist.subtitle, content: tasklist.content, link: '/task-list/'+tasklist._id});
+            }else{
+              return;
+            }
+            count++;
+          }
+        }
+      });
+    }
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 }
 
