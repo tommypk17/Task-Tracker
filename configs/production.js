d211c80a36d72206c9598e58ee100b81006d93d3
Tommy Kovalchuk <34948289+tommypk17@users.noreply.github.com>
2020-10-17 18:29:56 -0400

    Create production.js

---
+++ configs/production.js
@@ -0,0 +1,10 @@
+let prodConfig = {
+    hostname: 'localhost',
+    port: 3000,
+    database: {
+        uri: 'mongodb+srv://Task-Tracker:T@$kTr@<keR@mini-projects.e4wgm.gcp.mongodb.net/Task-Tracker?retryWrites=true&w=majority',
+        secret: 'T@$kTr@<keR'
+    }
+};
+
+module.exports = prodConfig;
